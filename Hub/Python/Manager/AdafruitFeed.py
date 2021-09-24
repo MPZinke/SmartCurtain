7f311b8bad76b790b82194996ec348ff244e5c3b
mpzinke <mathew.zinke@hotmail.com>
2021-09-24 09:39:03 -0500

    Hub: adds headers for Adafruit feed

--- Hub/Python/Manager/AdafruitFeed.py
+++ Hub/Python/Manager/AdafruitFeed.py
@@ -5,13 +5,27 @@ __author__ = "MPZinke"
 ########################################################################################################################
 #                                                                                                                      #
 #   created by: MPZinke                                                                                                #
-#   on ..                                                                                                              #
+#   on 2021.09.23                                                                                                      #
 #                                                                                                                      #
 #   DESCRIPTION:                                                                                                       #
 #   BUGS:                                                                                                              #
 #   FUTURE:                                                                                                            #
 #                                                                                                                      #
 ########################################################################################################################
+
+
+from Adafruit_IO import MQTTClient;
+from datetime import datetime, timedelta;
+from time import sleep;
+import warnings;
+from warnings import warn as Warn;
+
+from Class import ZWidget;
+from Manager.ManagerGlobal import *;
+from Other.Global import *;
+from Other.Global import warning_message;
+from Other.Logger import log_error;
+
 
 
 def main():
