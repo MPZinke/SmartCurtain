03af57138ae2c1757bf18cd4c6a7a641675dc99d
mpzinke <mathew.zinke@hotmail.com>
2021-10-06 10:16:04 -0500

    Hub: Progress on AdafruitFeed conversion

--- Hub/Python/Manager/AdafruitFeed.py
+++ Hub/Python/Manager/AdafruitFeed.py
@@ -28,8 +28,50 @@ from Other.Logger import log_error;
 
 
 
-def main():
+class AdafruitIO(ZWidget):
+	def __init__(self, System):
+		ZWidget.__init__(self, "AdafruitIO", System);
+
+		warnings.formatwarning = warning_message;
 
 
-if __name__ == "__main__":
-	main()
+	def _curtain_list(self):
+		curtains = self._System.Curtains();
+		return [curtains[curtain_id] for curtain_id in curtains];
+
+
+	def _curtain_for_feed_id(feed_id):
+		curtains = self._curtain_list();
+		for curtain in curtains:
+			if(curtain.CurtainOptionKeyValue(CurtainOptionKeyValue)):
+				return curtain;
+
+		return None;
+
+
+	def _activate(self, client, feed_id, position_payload):
+		try:
+			curtain = self._curtain_for_feed_id(feed_id);
+			if(not curtain): raise Exception(f"Feed ID: {feed_id} not found");
+
+		except Exception as error:
+			log_error(error);
+
+
+	def _connect(self, client):
+		for curtain in self._curtain_list():
+			# if curtain has CurtainOption for AdafruitIO && CurtainOption for AdafruitIO has CurtainOptionKeyValue:
+				# connect
+			if(curtain.CurtainOption(self.))
+
+
+	def _loop_process(self):
+		client = MQTTClient()  #TODO: populate
+
+		client.on_connect = self._connect;
+		client.on_disconnect = self._disconnect;
+		client.on_message = self._activate;
+		client.on_subscribe  = self._null
+		
+		client.connect();
+		client.loop_blocking();
