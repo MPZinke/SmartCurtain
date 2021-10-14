c2f820cf8e331551d741cb4a3534fec19ba630cd
mpzinke <mathew.zinke@hotmail.com>
2021-10-14 14:53:59 -0500

    Hub: Logger now outputs JSON formatted errors

--- Hub/Python/Other/Logger.py
+++ Hub/Python/Other/Logger.py
@@ -15,6 +15,7 @@ __author__ = "MPZinke"
 
 
 from datetime import datetime;
+from json import dumps as json_dumps;
 from sys import stderr;
 import traceback;
 from typing import Union;
@@ -22,19 +23,24 @@ from typing import Union;
 
 # Writes error to log file. If issue occurs writing error, error is sent to stderr along with previous function call.
 # Takes error object or string.
-# Tries to traceback error. Creates pipe delimited error message. Writes error to log (or stderr).
-# Returns whether successfully wrote to log
+# Tries to traceback error. Creates json error message. Writes error to log (or stderr).
+# Returns whether successfully written to log.
 def log_error(error : Union[Exception, str]) -> bool:
 	try:
-		try: module = traceback.format_exc().split("\n")[-4].strip();
+		try: module = traceback.format_exc();
 		except: module = "Unknown traceback";
-		error_message = "%s|%s|%s\n" % (datetime.now().strftime("%H:%M:%S"), module, error);
+		error_data = {"time": datetime.now().strftime("%H:%M:%S"), "module": module, "error": str(error)};
+		error_message = json_dumps(error_data, indent=4);
+
+		print(error_message);
 
 		log_name = "/usr/SmartCurtain/Logs/"+datetime.now().strftime("%Y.%m.%d")+".log";
 		with open(log_name, "a") as log: log.write(error_message);
-		print(error_message);
+
 		return True;
+
 	except Exception as log_error_error:
 		from inspect import stack;
 		print(stack()[1][3]+": ", error, file=stderr);
+
 		return False;
