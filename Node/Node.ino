32cac53121bc3f6f7910a762533ab5afb55a5382
mpzinke <mathew.zinke@hotmail.com>
2022-03-16 20:24:31 -0500

    Node: Removes encode code

--- Node/Node.ino
+++ Node/Node.ino
@@ -22,6 +22,7 @@
 ***********************************************************************************************************************/
 
 
+#include <ArduinoJson.h>
 #include <esp_wifi.h>
 #include <SPI.h>
 
@@ -29,13 +30,18 @@
 #include "Headers/Config.hpp"
 #include "Headers/Global.hpp"
 
-#include "Headers/Automation.hpp"
-#include "Headers/Encoder.hpp"
+#include "Headers/C_String.hpp"
+#include "Headers/Curtain.hpp"
+#include "Headers/Event.hpp"
+#include "Headers/Exceptions.hpp"
 #include "Headers/Hardware.hpp"
+#include "Headers/Movement.hpp"
+#include "Headers/Request.hpp"
 
 
-TaskHandle_t automation_task;
-TaskHandle_t encoder_task;
+StaticJsonDocument<JSON_BUFFER_SIZE> decode_json();
+void case_move(StaticJsonDocument<JSON_BUFFER_SIZE>& json_document);
+void case_update(StaticJsonDocument<JSON_BUFFER_SIZE>& json_document);
 
 
 void setup()
@@ -58,18 +64,113 @@ void setup()
 	while(WiFi.status() != WL_CONNECTED) delay(500);
 
 	Global::server.begin();
-
-//TODO: move completely closed if endstop
-	// Task Params: Function to implement the task, Name of the task, Stack size in words, Task input parame, 
-	//  Priority of the task, Task handle to keep track of created task, Core where the task should run
-	xTaskCreatePinnedToCore(Automation::automation_loop, "Automation", 10000, NULL, 0, &automation_task, 0);
-	if(Config::Hardware::ENCODER)
-	{
-		xTaskCreatePinnedToCore(Encoder::encoder_loop, "Encoder", 10000, NULL, 1, &encoder_task, 1);
-	}
 }
 
 
 void loop()
-{}
+{
+	Hardware::disable_motor();  // don't burn up the motor
+		
+	StaticJsonDocument<JSON_BUFFER_SIZE> json_document = decode_json();
 
+	// If curtain information, update Global::curtain information
+	if(json_document.containsKey(Request::Literal::JSON::Key::CURTAIN))
+	{
+		Global::curtain.update(json_document);
+	}
+
+	// Call action for QUERY_TYPE
+	switch(Request::id_for_value(json_document[Request::Literal::JSON::Key::QUERY_TYPE]))
+	{
+		// Update information about curtain
+		case Request::Literal::JSON::Value::UPDATE_ID:
+		{
+			case_update(json_document);
+			// Fall through to STATUS_ID
+		}
+
+		// Return information about Curtain
+		case Request::Literal::JSON::Value::STATUS_ID:
+		{
+			Request::send_status_and_stop_client();
+			break;
+		}
+
+		// Reset curtain by moving it from alleged current position to close to actual current position.
+		case Request::Literal::JSON::Value::RESET_ID:
+		{
+			Movement::EndstopGuarded::move_and_reset();
+			break;
+		}
+
+		// Move to position
+		case Request::Literal::JSON::Value::MOVE_ID:
+		{
+			case_move(json_document);
+			break;
+		}
+
+		default:
+		{
+			Exceptions::throw_HTTP_404("Unknown query type");
+		}
+	}
+
+	setjmp(Global::jump_buffer);
+
+	delay(700);
+}
+
+
+// RETURN: The JSON Document object.
+// THROWS: HTTPS_Exceptions
+StaticJsonDocument<JSON_BUFFER_SIZE> decode_json()
+{
+	Global::client = Request::wait_for_request();
+
+	// bad message: retry later
+	String json_buffer = Request::read_request_data_into_buffer();
+	if(!json_buffer.length())
+	{
+		Exceptions::throw_HTTP_204("Could not read Request into json_buffer");
+	}
+
+	// Decode JSON
+	StaticJsonDocument<JSON_BUFFER_SIZE> json_document;
+	if(!deserializeJson(json_document, json_buffer))
+	{
+		Exceptions::throw_HTTP_400("Could not decode json");
+	}
+
+	return json_document;
+}
+
+
+// ——————————————————————————————————————————————————— CASES  ——————————————————————————————————————————————————— //
+
+void case_move(StaticJsonDocument<JSON_BUFFER_SIZE>& json_document)
+{
+	if(!json_document.containsKey(Request::Literal::JSON::Key::EVENT))
+	{
+		String error_message = String("Missing key: \"") + Request::Literal::JSON::Key::EVENT
+		  + "\" for QUERY_TYPE: \"" + Request::Literal::JSON::Value::MOVE + "\"";
+		Exceptions::throw_HTTP_404(error_message.c_str());
+	}
+
+	Request::respond_with_json_and_stop(Request::Literal::Responses::VALID);
+	JsonObject event_object = json_document[Request::Literal::JSON::Key::EVENT];
+	Event::Event event(event_object);
+
+	Movement::activate(event);
+}
+
+
+void case_update(StaticJsonDocument<JSON_BUFFER_SIZE>& json_document)
+{
+	using namespace Request::Literal::JSON;
+
+	if(json_document.containsKey(Key::HUB_IP))
+	{
+		C_String::copy(json_document[Key::HUB_IP], Config::Network::HUB_HOST_STR);
+	}
+}
