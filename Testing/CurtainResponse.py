07a6804f5d984971713c7d1c7d1c34bfe3d44b7b
mpzinke <mathew.zinke@gmail.com>
2022-07-17 14:39:52 -0500

    Starts merging Hub working with MotorThread test

--- Testing/CurtainResponse.py
+++ Testing/CurtainResponse.py
@@ -34,10 +34,10 @@ def request(data: dict) -> None:
 	print()
 
 
-# request({"query type": "status"})
+request({"query type": "status"})
 # request({"query type": "move", "event": {"id": 4294967295, "percentage": 100}})
 # request({"query type": "move", "event": {"id": 4294967295, "percentage": 0}})
 # request({"query type": "move"})
 # request({"query type": "reset"})
-request({"wrong": "wrong"})
-request({})
+# request({"wrong": "wrong"})
+# request({})
