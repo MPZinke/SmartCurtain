faf73395b8635ca7a8d381ca197a5336e9c8d30f
mpzinke <mathew.zinke@gmail.com>
2023-04-29 21:08:14 -0500

    Fixes Wifi client duplicate importing

--- Headers/Message.hpp
+++ Headers/Message.hpp
@@ -15,7 +15,6 @@
 
 
 #include <ArduinoJson.h>
-#include <WiFiClient.h>
 
 
 #include "Config.hpp"
--- src/Event.cpp
+++ src/Event.cpp
@@ -14,6 +14,7 @@
 
 #include "../Headers/Event.hpp"
 
+
 #include "../Headers/Global.hpp"
 
 #include "../Headers/Curtain.hpp"
