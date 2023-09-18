aca6e46ea8bebff30c5178e027fa89ae13edc8c5
mpzinke <mathew.zinke@gmail.com>
2023-09-17 23:28:09 -0500

    Hello World on frontend running

--- Hub/Frontend/Frontend.py
+++ Hub/Frontend/Frontend.py
@@ -1,10 +1,24 @@
 
 
 import mpzinke
+import os
+import pathlib
+
+
+import Routes
+
+
+FRONTEND_DIRECTORY = str(pathlib.Path(__file__).parent)
+TEMPLATE_DIRECTORY = os.path.join(FRONTEND_DIRECTORY, "HTML", "Root")
+STATIC_DIRECTORY = os.path.join(FRONTEND_DIRECTORY, "HTML", "Static")
 
 
 def main():
-	server = mpzinke.Server(name="SmartCurtain-Frontend")
+	server = mpzinke.Server(name="SmartCurtain-Frontend",
+		template_folder=TEMPLATE_DIRECTORY, static_folder=STATIC_DIRECTORY
+	)
+
+	server._app.add_url_rule("/", "/", Routes.Root.index, ["GET"])
 
 	server(port=80)
 
